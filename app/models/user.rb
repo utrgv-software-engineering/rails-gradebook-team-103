db0382a4701ff180e3e729bbc8c8672d1faabffa
Roy Villarreal <roy.villarreal01@utrgv.edu>
2022-12-11 05:45:15 +0000

    added tests for teacher and TA

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,3 +4,15 @@ class User < ApplicationRecord
   devise :database_authenticatable, 
          :recoverable, :rememberable, :validatable
 end
+class teacher < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, 
+         :recoverable, :rememberable, :validatable
+end
+class ta < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, 
+         :recoverable, :rememberable, :validatable
+end
