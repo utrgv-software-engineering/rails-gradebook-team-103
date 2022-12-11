803fde0788120ca18ccf841eb630bc7c12d96ec4
Kevin Grajeda <kevin.grajeda01@utrgv.edu>
2022-12-11 05:13:19 +0000

    JRK-2

--- app/models/grade.rb
+++ app/models/grade.rb
@@ -3,6 +3,7 @@ class Grade < ApplicationRecord
     validates_presence_of :student_name
     validates_presence_of :student_grade
     validate :grade_must_be_greater_than_or_equal_to_zero
+    validate :id_must_be_5_characters_long
 
     #custom validator
     #makes sure grade is larger than zero
@@ -14,6 +15,15 @@ class Grade < ApplicationRecord
             end
             if student_grade > 100
                 errors.add(:student_grade, "can't be greater than 100")
+            end
+        end
+    end
+
+    def id_must_be_5_characters_long
+        # Checks for students ID to be equal or greater than 5 characters
+        if student_id != nil
+            if student_id.length < 5
+                errors.add(:student_id, "Can't be less than 5 characters")
             end
         end
     end
